7f94efa53ce2a7b25c194d8622dbf88de1581ffb
Purdey Chambraud <purdey.chambraud@inwebo.com>
2018-11-03 00:50:48 +0100

    removing item from specific list when item is already in it

--- src/stores/ListsStore.jsx
+++ src/stores/ListsStore.jsx
@@ -1,6 +1,10 @@
  import {autorun, observable, action} from "mobx"
 // import  from "/src/listsfakedata.mobx"
 import fakeState from "../listsfakedata.mobx.js";
+import {
+    setInLocalStorage,
+    getFromLocalStorage
+  } from "../services/localStorageService";
 
  class ListsStore {
      @observable lists = []
@@ -14,8 +18,20 @@ import fakeState from "../listsfakedata.mobx.js";
      }
      @action.bound
      addItemInThisList = (listId, itemId) => {
-         return this.lists[listId].itemsInThisList.push(itemId);
+         let isThisItemInThisList = this.lists[listId].itemsInThisList.some(item =>(itemId === item ));
+         console.log("isThisItemInThisList",isThisItemInThisList);
+         if(isThisItemInThisList) {
+             console.log("need to be removed from list")
+             const index = this.lists[listId].itemsInThisList.indexOf(itemId)
+             //TODO: manage change of color of icons list
+             return this.lists[listId].itemsInThisList.splice(index,1);
+         } else {
+            return this.lists[listId].itemsInThisList.push(itemId);
+         }
      }
+
+
+    
  } 
 
  const listsStore = window.listsStore = new ListsStore();
@@ -25,7 +41,11 @@ export default listsStore;
 
  autorun(() => {
     console.log("autorun listsStore ",listsStore)
-
+    let lists = listsStore.lists;
+    Object.keys(lists).map(list => {
+        console.log("list autorun",lists[list])
+        setInLocalStorage(lists[list].id,lists[list])
+    })
  })
 
 // this.todos.push(new TodoModel(title));
